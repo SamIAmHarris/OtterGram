7efaf1f95ce54da59f4cb1eb82cef2d753db81bc
Sam Harris <SamIAm@Sams-MacBook-Pro.local>
2016-12-03 10:54:15 -0600

    gold challenge: make one of the random thumbnails images different than the thumbnail at start. And each time they are clicked set a new random thumbnail to have the secret image

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var TACO_CAT_SRC = 'https://cdn.shopify.com/s/files/1/0032/7882/products/tacocat_art.png?v=1447870796';
 
 
 function setDetails(imageUrl, imageTitle) {
@@ -21,6 +22,18 @@ function titleFromThumb(thumbnail) {
   return thumbnail.getAttribute('data-image-title');
 }
 
+function resetThumbnailImageAttribute() {
+  'use-strict';
+  var thumbnails = getThumbnailsArray();
+  thumbnails.forEach(setSrcAttributeToDataImageUrl);
+}
+
+function setSrcAttributeToDataImageUrl(thumbnail) {
+  'use-strict';
+  var originalSrc = thumbnail.getElementsByClassName('thumbnail-image')[0].getAttribute('src');
+  thumbnail.setAttribute('data-image-url', originalSrc);
+}
+
 function setDetailsFromThumb(thumbnail) {
   'use strict';
   var imageSrc = imageFromThumb(thumbnail);
@@ -33,6 +46,8 @@ function addThumbClickHandler(thumb) {
   thumb.addEventListener('click', function (event) {
       event.preventDefault();
       setDetailsFromThumb(thumb);
+      resetThumbnailImageAttribute();
+      changeRandomThumbnailLink(getThumbnailsArray());
   });
 }
 
@@ -44,10 +59,17 @@ function getThumbnailsArray() {
   return thumbnailArray;
 }
 
+function changeRandomThumbnailLink(thumbnails) {
+  var randomNumber = Math.floor(Math.random() * 5);
+  var randomThumbnail = thumbnails[randomNumber];
+  randomThumbnail.setAttribute('data-image-url', TACO_CAT_SRC);
+}
+
 function initializeEvents() {
   'use-strict';
   var thumbnails = getThumbnailsArray();
   thumbnails.forEach(addThumbClickHandler);
+  changeRandomThumbnailLink(thumbnails);
 }
 
 initializeEvents();
