2e0cdc31391d42bc51f61b963d9599e5e63d1e55
Sam Harris <SamIAm@Sams-MacBook-Pro.local>
2016-12-02 20:39:38 -0600

    add click functionality to each thumbnail to set the main image/title to that thumbnails object info

--- scripts/main.js
+++ scripts/main.js
@@ -27,3 +27,27 @@ function setDetailsFromThumb(thumbnail) {
   var title = titleFromThumb(thumbnail);
   setDetails(imageSrc, title);
 }
+
+function addThumbClickHandler(thumb) {
+  'use-strict';
+  thumb.addEventListener('click', function (event) {
+      event.preventDefault();
+      setDetailsFromThumb(thumb);
+  });
+}
+
+function getThumbnailsArray() {
+  'use-strict';
+  var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+  //convert the NodeList to an array
+  var thumbnailArray = [].slice.call(thumbnails);
+  return thumbnailArray;
+}
+
+function initializeEvents() {
+  'use-strict';
+  var thumbnails = getThumbnailsArray();
+  thumbnails.forEach(addThumbClickHandler);
+}
+
+initializeEvents();
